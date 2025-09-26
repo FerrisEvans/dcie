1bbf7d5ab360aac6f6967c3745e6a3a1d7e24883
FerrisEvans <ferris.evans@outlook.com>
2025-09-26 16:50:50 +0800

    api routes

--- app/tools.py
+++ app/tools.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
+from starlette import status
 
 from core.logger import log
 router = APIRouter(prefix="/tools")
@@ -7,7 +9,13 @@ log.success("tools router initialized")
 
 # import engine.reader
 
-@router.post("/uploadfile/")
+@router.post("/uploadfile/", status_code=status.HTTP_200_OK)
 async def create_upload_file(file: UploadFile = File(...)):
     contents = await file.read()
-    return {"filename": file.filename, "contents": contents}
+    return JSONResponse({"filename": file.filename, "contents": contents})
+
+
+@router.get("/hello")
+async def hello():
+    log.info("hello")
+    return JSONResponse({"hello": "world"})
--- main.py
+++ main.py
@@ -10,6 +10,17 @@ from core.conn import master_async_engine, get_redis
 from core.exceptions import register_exception
 from core.logger import setup_logging, log
 
+async def run_app():
+    await setup_logging()
+    config = uvicorn.Config(
+        "main:app",
+        host="0.0.0.0",
+        port=8100,
+        log_config=None
+    )
+    server = uvicorn.Server(config)
+    await server.serve()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
@@ -27,12 +38,10 @@ async def lifespan(app: FastAPI):
         log.success("Postgres pool closed")
         app.redis.close()
         log.success("Redis closed")
-
 app = FastAPI(
     title="dcie",
     description="document compliance identification engine",
     version="0.1.0",
-    root_path=conf.api,
     docs_url="/docs",
     contact={
         "name": "Ferris",
@@ -41,31 +50,24 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
 
-async def run_app():
-    await setup_logging()
-    # 注册全局异常
-    register_exception(app=app)
-    # 跨域
-    if conf.http.enable_cors:
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=conf.http.allow_origins,
-            allow_credentials=conf.http.allow_credentials,
-            allow_methods=conf.http.allow_methods,
-            allow_headers=conf.http.allow_headers,
-        )
-    # api
-    for r in routers:
-        app.include_router(r)
-    config = uvicorn.Config(
-        "main:app",
-        host="0.0.0.0",
-        port=8100,
-        log_config=None
+# 注册全局异常
+register_exception(app=app)
+# 跨域
+if conf.http.enable_cors:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=conf.http.allow_origins,
+        allow_credentials=conf.http.allow_credentials,
+        allow_methods=conf.http.allow_methods,
+        allow_headers=conf.http.allow_headers,
     )
-    server = uvicorn.Server(config)
-    await server.serve()
+# api
+for r in routers:
+    app.include_router(r)
 
 if __name__ == '__main__':
     import asyncio
